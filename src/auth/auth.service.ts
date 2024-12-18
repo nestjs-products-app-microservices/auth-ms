dba3470793b65186910119c48b2293c2b00cd322
renato-av <renato.avela.17@gmail.com>
2024-12-18 18:32:20 -0500

    add Prisma integration for MongoDB, update auth service to connect to the database, and configure environment variables

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common'
 import { RegisterUserDto } from './dto/register-user.dto'
 import { LoginUserDto } from './dto/login-user.dto'
+import { PrismaClient } from '@prisma/client'
 
 @Injectable()
-export class AuthService {
+export class AuthService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(AuthService.name)
+
+  onModuleInit() {
+    this.$connect()
+    this.logger.log('Connected to the MongoDB database')
+  }
 
   registerUser(registerUserDto: RegisterUserDto) {
     return {
